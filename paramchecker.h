c20043936b60e8b5e65cbdee58649245e90ab412
Athul K <athulkharsham@gmail.com>
2020-07-28 16:11:14 +0530

    Update paramchecker.h

--- paramchecker.h
+++ paramchecker.h
@@ -1,2 +1,5 @@
 bool ifVitalLimitsOk(float value, float minValue, float maxValue);
-bool ifVitalsOK(float bpm, float spo2, float respRate);
+// bool ifVitalsOK(float bpm, float spo2, float respRate);
+bool isVitalsOkBpm(value);
+bool isVitalsOkSpo2(value);
+bool isVitalsOkResprate(value);
