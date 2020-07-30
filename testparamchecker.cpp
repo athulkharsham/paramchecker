47d4be4eaba8db12f12f64763d4ae81e64e37f20
Athul K <athulkharsham@gmail.com>
2020-07-30 15:36:03 +0530

    refactoring the code

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -1,18 +1,24 @@
 #include "paramchecker.h"
 #include <gtest/gtest.h>
-
-TEST(VitalsTest, BPM) {
-    ASSERT_EQ(true, isVitalsOkBpm(100));
+ 
+TEST(VitalsTest, when_a_vital_is_in_range_vital_range_check_is_ok) {
+    VitalRangeCheck vitalChecker(60, 100);
+    ASSERT_EQ(vitalChecker.measurementIsOk(70), true);
 }
 
-TEST(VitalsTest, SPO2) {
-    ASSERT_EQ(false, isVitalsOkBpm(40));
+TEST(VitalsTest, when_a_vital_is_off_limit_it_is_reported_with_vital_id) { 
+    std::vector<Measurement> measurements = {
+        {bpm, 100},
+        {spo2, 50},
+        {respRate, 50},
+    };
+    auto results = vitalsAreOk(measurements);
+    ASSERT_EQ(results.size(), sizeof(measurements)/sizeof(measurements[0]));
+    ASSERT_EQ(results[spo2], false);
+    ASSERT_EQ(results[bpm], true);
+    ASSERT_EQ(results[respRate], true);
 }
-
-TEST(VitalsTest, RESPRATE) {
-    ASSERT_EQ(true, isVitalsOkResprate(50));
-}
-                                        
+ 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
