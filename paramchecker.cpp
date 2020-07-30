07663468c8b9018f5c349ccea4ed5afefea99501
Athul K <athulkharsham@gmail.com>
2020-07-30 15:34:14 +0530

    refactoring

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,48 +1,19 @@
+#include "paramchecker.h"
+#include <vector>
+#include <iostream>
 
-#define bpm_low 70
-#define bpm_high 150
-#define sp02_low 80
-#define sp02_high 100
-#define resprate_low 30
-#define resprate_high 60
+IVitalCheck* vitalCheckers[] = {
+  [bpm] = new VitalRangeCheck(70, 150),
+  [spo2] = new VitalRangeCheck(80, 100),
+  [respRate] = new VitalRangeCheck(30, 60),
+  [avgECG] = new VitalValueCheck(0),
+};
 
-typedef   bool (*Operation)(float a);
-
-typedef struct
-{
-  Operation  vitalChecker; // funtion pointer 
-}vitals
-vitals vitals_obj = {isVitalsOkBpm,
-                     isVitalsOkSpo2,
-                     isVitalsOkResprate
-                     };
-bool ifVitalLimitsOk(float value, float minValue, float maxValue)
-{
-  bool retVal = true;
-  
-  if( (value < minValue) || (value > maxValue) )
-  {
-    retVal = false;
+std::vector<bool> vitalsAreOk(const std::vector<Measurement>& measurements) {
+  std::vector<bool> results;
+  for(auto t = measurements.begin(); t != measurements.end(); t++) {
+    bool vitalResult = vitalCheckers[t->id]->measurementIsOk(t->measured_value);
+    std::cout << "Vital-check result is " << vitalResult << std::endl;
   }
-  return retVal;
-}
-
-/* bool ifVitalsOK(float bpm, float spo2, float respRate)
-{
-  return (ifVitalLimitsOk(bpm,bpm_low,bpm_high) &&
-           ifVitalLimitsOk(spo2,sp02_low,sp02_high) &&
-           ifVitalLimitsOk(respRate,resprate_low,resprate_high) );
-}
-*/
-bool isVitalsOkBpm(float value)
-{
-  return ( ifVitalLimitsOk(value, bpm_low, bpm_high) );
-}
-bool isVitalsOkSpo2(float value)
-{
-  return ( ifVitalLimitsOk(value,sp02_low,sp02_high) );
-}
-bool isVitalsOkResprate(float value)
-{
-  return ( ifVitalLimitsOk(value,resprate_low,resprate_high) );
+  return results;
 }
